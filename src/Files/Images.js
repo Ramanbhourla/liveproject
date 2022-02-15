763388b947efc002fbf3656f0bfb35fdce8c3d78
Ramandeep Singh <98514268+Ramanbhourla@users.noreply.github.com>
2022-02-15 13:52:44 +0530

    Update Images.js

--- src/Files/Images.js
+++ src/Files/Images.js
@@ -1,7 +1,7 @@
 const Images =[
     {
         id: 0,
-        source:'images/sports.jpg',
+        source:'https://media.istockphoto.com/photos/closeup-of-athletic-muscular-man-picture-id487768398?k=20&m=487768398&s=612x612&w=0&h=Wi5fixxXP8dYELLXOVMsVudAXlBMe23tG4H3H49XZCU=',
         tittle: "SPORTS"
     },
     { 
