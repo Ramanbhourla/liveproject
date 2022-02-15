6a9b7ec80c9cda9ca0a3d04dcbd7851d52c07d5b
Ramandeep Singh <98514268+Ramanbhourla@users.noreply.github.com>
2022-02-15 12:54:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ import App from "./app";
 import "./Files/index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-//bootsrap installed with  css we can just import it and use its compontents from its offical site...
-//bootsrap is all about classes...
-//
-// import { createStore } from "redux";
-// import RootReducer from "./ServiceR/Reducers/RootReduser";
-// import { Provider } from "react-redux";
 
-// const Store = createStore(RootReducer);
-//   console.log(Store)
 reactDOM.render(
   <BrowserRouter>
     <App />
